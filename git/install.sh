2537cab4b35bc67ff7250737157bfccba964c9af
Shijiang Wei <mountkin@gmail.com>
2018-07-11 10:25:58 +0800

    add a helper script to add ELF executables in .gitignore
    Signed-off-by: Shijiang Wei <mountkin@gmail.com>

--- git/install.sh
+++ git/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get install -y exuberant-ctags vim
 cp git-diff-wrapper /usr/bin
+cp git-ignore /usr/bin
 [ -d ~/.git_template ] || mkdir ~/.git_template
 cp -r .git_template/* ~/.git_template
 cp .gitconfig ~/
